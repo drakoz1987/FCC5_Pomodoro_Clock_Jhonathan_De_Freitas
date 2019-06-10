712122efeabb29c97df6defbb4dcc20acb4644c1
De Freitas <drakoz1987@gmail.com>
2019-06-10 17:45:08 -0400

    Added build folder

---
+++ build/precache-manifest.8817df96d545c1788ce1876ba0b05e5e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b86b2071fa531e652d66ebab566d47f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0b893f5bb136a590b6",
+    "url": "/static/js/2.7000bb6f.chunk.js"
+  },
+  {
+    "revision": "434d9f17162ba2aaf146",
+    "url": "/static/js/main.d3d26959.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
